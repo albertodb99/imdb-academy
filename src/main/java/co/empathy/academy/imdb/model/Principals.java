a0da798cf7acf9a45fea53e5839b4e7b983f89c2
Alberto Díez <uo266536@uniovi.es>
2022-05-26 12:53:49 +0200

    fixed something related with indexing

--- src/main/java/co/empathy/academy/imdb/model/Principals.java
+++ src/main/java/co/empathy/academy/imdb/model/Principals.java
@@ -1,10 +1,13 @@
 package co.empathy.academy.imdb.model;
 
 import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonObjectBuilder;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Principals {
     private static final int NCONST = 2;
@@ -12,18 +15,28 @@ public class Principals {
 
     private Principals() {}
 
-
     public static void addPrincipals(List<String> principalsLines, JsonObjectBuilder builder, List<String> akasHeaders) {
         var principalsArray = Json.createArrayBuilder();
         for(String principalLine : principalsLines) {
             var fields = principalLine.split("\t");
 
+            JsonArrayBuilder charactersJsonBuilder = Json.createArrayBuilder();
+            toJsonArray(fields[CHARACTERS], charactersJsonBuilder);
             principalsArray.add(Json.createObjectBuilder()
                     .add("name", Name.addName(fields[NCONST]))
-                    .add(akasHeaders.get(CHARACTERS), fields[CHARACTERS])
+                    .add(akasHeaders.get(CHARACTERS), charactersJsonBuilder)
             );
         }
 
         builder.add("principals", principalsArray);
+    }
+
+    private static void toJsonArray(String string, JsonArrayBuilder builder) {
+        if(!string.equals("\\N")) {
+            String noBraces = string.substring(1, string.length() - 1);
+            Arrays.stream(noBraces.split(",")).map(s -> s.substring(1, s.length() - 1)).forEach(builder::add);
+        } else {
+            builder.add(string);
+        }
     }
 }
